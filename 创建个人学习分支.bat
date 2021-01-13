b511ee331aefb40eae2fd6c039ecc9bf3966f1ad
wu_jh <wu_jh@piec.com.cn>
2021-01-13 17:43:35 +0800

    【更新】 * “创建个人学习分支.bat”：  - 优化脚本  - 加入空字符串识别

--- 创建个人学习分支.bat
+++ 创建个人学习分支.bat
@@ -1,11 +1,12 @@
 @echo off
 
 ::统计学习人数和人员======================================================
+:PROGRAM_LEARNER_CALCULATION
 set TEMP_FILE=_temp_.txt
 git branch --list >> %TEMP_FILE%
 set /a BranchLength=0
 setlocal enabledelayedexpansion
-for /f %%i in (%TEMP_FILE%) do (
+for /f "tokens=1 delims=" %%i in (%TEMP_FILE%) do (
     set GetBranchName=%%i
 	echo !GetBranchName!|findstr "_Learning" >nul
 	if !errorlevel!==0 (
@@ -15,7 +16,7 @@ for /f %%i in (%TEMP_FILE%) do (
 echo 【当前参与学习总人数】：%BranchLength%
 echo 【当前参与学习的人员】——————————————————————————————————————————
 setlocal enabledelayedexpansion
-for /f %%i in (%TEMP_FILE%) do (
+for /f "tokens=1 delims=" %%i in (%TEMP_FILE%) do (
     set GetBranchName=%%i
 	echo !GetBranchName!|findstr "_Learning" >nul
 	if !errorlevel!==0 (
@@ -24,31 +25,40 @@ for /f %%i in (%TEMP_FILE%) do (
 )
 DEL %TEMP_FILE%
 
+
 ::创建个人学习分支======================================================
-set /p ip=【创建个人学习分支】请输入你的名字(例：WuJiaHong)：
+:PROGRAM_CREATE_BRANCH
+set /p User=【创建个人学习分支】请输入你的名字(例：WuJiaHong)：
 
-set BranchName=%ip%_Learning
-
-git branch %BranchName%>nul 2>nul
-
-if %errorlevel%==0 (
-	echo 【创建结果】SUCCESS，分支“%BranchName%”创建成功！！！
+set BranchName=%User%_Learning
+if defined User (
+	git branch !BranchName!>nul 2>nul
+	if !errorlevel!==0 (
+		echo 【创建结果】SUCCESS，分支“!BranchName!”创建成功！！！
+	) else (
+		echo 【创建结果】ERROR，分支“!BranchName!”已经存在！！！
+	)
 ) else (
-	echo 【创建结果】ERROR，分支“%BranchName%”已经存在！！！
+	echo 【创建结果】ERROR，名字为空！！！
+	GOTO PROGRAM_END
 )
+
 
 ::切换学习分支======================================================
-set /p Switch=【是否切换到该分支？（y/n）】：
-if %Switch%==y (
+:PROGRAM_SWITCH_BRANCH
+::set /p Switch=【是否切换到该分支？（y/n）】：
+::if %Switch%==y (
 
-	git checkout !BranchName!
-	if !errorlevel!==0 (
-		echo 【切换结果】SUCCESS：切换成功！！！
-	) else (
-		echo 【切换结果】ERROR：切换失败，请查看错误信息！！！
-	)
-	
+git checkout !BranchName!
+if !errorlevel!==0 (
+	echo 【切换结果】SUCCESS：分支切换成功！！！
+) else (
+	echo 【切换结果】ERROR：分支切换失败，请查看错误信息！！！
 )
+	
+::)
 echo 【当前分支】“%BranchName%”
 
+::程序结束======================================================
+:PROGRAM_END
 pause
