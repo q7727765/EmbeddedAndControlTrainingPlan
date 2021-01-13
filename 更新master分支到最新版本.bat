05b49c40d94db4e577d05a1fbc67bdd9fef05509
wu_jh <wu_jh@piec.com.cn>
2021-01-14 00:45:59 +0800

    【更新】 * “更新到最新版本.bat”：  - 改名为：“更新master分支到最新版本.bat”  - 添加一个http协议的remote：origin_http。从而兼容使用ssh协议上传和使用http协议更新本地

--- 更新master分支到最新版本.bat
+++ 更新master分支到最新版本.bat
@@ -1,7 +1,7 @@
 @echo off
 echo 【EmbeddedAndControlTranining更新程序】
-git remote update
-git pull origin master
+git remote update origin_http
+git pull origin_http master
 git reset --hard HEAD
 echo "——当前版本信息——"
 git tag
